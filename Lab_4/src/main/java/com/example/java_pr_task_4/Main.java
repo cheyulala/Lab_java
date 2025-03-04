e5fbca1e9df98fc39f293fb1f1f74f7d051c2ae8
cheyulala <cheyulala@gmail.com>
2025-03-04 17:01:09 +0200

    Main.java

---
+++ Lab_4/src/main/java/com/example/java_pr_task_4/Main.java
@@ -0,0 +1,25 @@
+package com.example.java_pr_task_4;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Сar dump");
+        stage.setWidth(400);
+        stage.setHeight(300);
+        stage.show();
+    }
+}
